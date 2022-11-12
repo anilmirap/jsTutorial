b3257aa48b3ed068c2e5275f8b6a0980116843b6
anilmirap <anilmirap@outlook.com>
2022-11-12 16:28:02 +0300

    varTypes

---
+++ js/varTypes.js
@@ -0,0 +1,12 @@
+var urunAdi = "iPhone 14"; //string
+let urunFiyat = 30999; //number
+
+console.log(typeof urunAdi);
+console.log(typeof urunFiyat);
+
+let sayi1 = "10";
+let sayi2 = "20";
+
+console.log(sayi1 + sayi2);
+console.log(Number(sayi1) + Number(sayi2));
+
